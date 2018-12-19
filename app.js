3a3757b12da141a74343b8a961fd7a55051fff9b
thierno sadou diallo <thierved01@gmail.com>
2018-12-19 17:11:51 +0000

    tried to solve it

--- app.js
+++ app.js
@@ -1,13 +1,50 @@
-    /**** PROBLEM 3 ****/
+   /**** PROBLEM 4  ****/
 
-    const searchInsert = function(nums, target) {
-        return (nums.concat([target])).sort((a,b) => {
-            return a - b;
-        }).indexOf(target);        
-    };
+// Complete the dynamicArray function below.
+    function dynamicArray(n, queries) {
+        const seqList = generateEmptyArr(n);
+        let lastAnswer = 0;
+
+        for (const q of queries) {
+            if (q[0] === 1) {
+                seqList[findSpecifiedIndex(q[1], lastAnswer, n)].push(q[2]);
+            } 
+            
+            if(q[0] === 2) {
+                let seq = seqList[findSpecifiedIndex(q[1], lastAnswer, n)];
+                lastAnswer = seq[q[2]%seq.length];
+                console.log(lastAnswer);
+            }
+        }
+    }
+     
+   
+   function generateEmptyArr(n) {
+        const arr2d = [];
+
+        for (let i = 0; i < n; i++) {
+            arr2d[i] = []
+        }
+
+        return arr2d;
+   }
+
+   function findSpecifiedIndex(x, lastAns, n) {
+       return x & (~lastAns) % n;
+   }
+   
+   
+   
+   /**** PROBLEM 3 ****/
+
+    // const searchInsert = function(nums, target) {
+    //     return (nums.concat([target])).sort((a,b) => {
+    //         return a - b;
+    //     }).indexOf(target);        
+    // };
 
 
-    console.log(searchInsert([1, 2, 3, 5, 10, 11, 15], 16));
+    // console.log(searchInsert([1, 2, 3, 5, 10, 11, 15], 16));
     
 
     /**** PROBLEM 2 ****/
